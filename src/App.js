fff38510114724acd89de5d10102ee5198430ce5
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-28 23:23:09 +0300

    d

--- src/App.js
+++ src/App.js
@@ -102,3 +102,16 @@ export default App;
 
 
 
+
+// import Rating from '@mui/material/Rating';
+// import Stack from '@mui/material/Stack';
+
+// function App() {
+//   return (
+//     <Stack spacing={1}>
+//       <Rating name="half-rating" defaultValue={2.5} precision={0.5} sx={{backgroundColor:"red"}}/>
+//       <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+//     </Stack>
+//   );
+// }
+// export default  App
